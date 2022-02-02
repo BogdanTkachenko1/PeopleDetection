32b631a49ad37132bb69a2108fc3c652ca170255
Bohdan Tkachenko <tkachenko-b@bubuntu-lx.nixsolutions.com>
2022-02-02 18:44:09 +0200

    Added color parameter to script

--- CrowdDetection/model/emd_simple/masking_script.py
+++ CrowdDetection/model/emd_simple/masking_script.py
@@ -18,7 +18,7 @@ top_k = 2
 
 
 class PeopleFillingModel:
-    def __init__(self, weights_path, color, threshold):
+    def __init__(self, weights_path, color, threshold, fill):
         if not os.path.exists(weights_path):
             raise FileNotFoundError('Weights file was not found')
 
@@ -26,11 +26,13 @@ class PeopleFillingModel:
             raise ValueError('Detection threshold must be in range (0; 1)')
 
         self.threshold = threshold
-        
-        if not (0 <= color <= 255):
-            raise ValueError('Color must be in range [0; 255]')
-        
+
+        if len(color) != 3 or min(color) < 0 or max(color) > 255:
+            raise ValueError('Color must be tuple of 3 integers in range [0; 255]')
+
         self.color = color
+
+        self.fill = fill
 
         self.network = network.Network()
         self.network.eval()
@@ -84,8 +86,13 @@ class PeopleFillingModel:
             predicted_boxes = np.apply_along_axis(self.get_coordinates, 1, predicted_boxes[keep][:, :-1])
 
         result_image = np.asarray(original_image, dtype=np.int32)
+
         for box in predicted_boxes:
-            result_image[round(box[1]):round(box[3]), round(box[0]):round(box[2])] = self.color
+            box = list(map(round, box))
+            if self.fill:
+                result_image = cv2.rectangle(result_image, (box[0], box[3]), (box[2], box[1]), self.color, 1)
+            else:
+                result_image[box[1]:box[3], box[0]:box[2]] = self.color
 
         return result_image
 
@@ -95,11 +102,12 @@ if __name__ == '__main__':
     parser.add_argument('--resume_weights', '-w', default=None, type=str)
     parser.add_argument('--source_folder_path', '-s', default=None, type=str)
     parser.add_argument('--destination_folder_path', '-d', default=None, type=str)
-    parser.add_argument('--color', '-c', default=0, type=int)
+    parser.add_argument('--color', '-c', default=0, nargs='+', type=int)
+    parser.add_argument('--fill', '-f', default=True, type=bool)
     parser.add_argument('--thresh', '-t', default=0.3, type=float)
     args = parser.parse_args()
 
-    model = PeopleFillingModel(args.resume_weights, args.color, args.thresh)
+    model = PeopleFillingModel(args.resume_weights, args.color, args.thresh, args.fill)
 
     source_folder_path = args.source_folder_path
     source_folder_name = source_folder_path.split('/')[-1]
